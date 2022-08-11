076d4aa2bc25797ad00b0b154ac47a5d58acaa1d
El mahdi Abdelhadi <abdelhadimahdi88@gmail.com>
2022-08-11 15:10:00 +0100

    completed

--- cpp_module_08/ex01/Span.cpp
+++ cpp_module_08/ex01/Span.cpp
@@ -1,9 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eabdelha <eabdelha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 08:34:30 by eabdelha          #+#    #+#             */
+/*   Updated: 2022/08/11 10:30:44 by eabdelha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Span.h"
 
-Span::Span()
-{
-    std::cout << "Span" << " default constructor called." << std::endl;
-}
+Span::Span(size_t n) : size(n) {}
 
 Span::Span(const Span &span)
 {
@@ -14,11 +23,55 @@ Span &Span::operator = (const Span &span)
 {
     if (this == &span)
         return (*this);
-    
+    numbers = span.numbers;
+    size = span.size;
     return (*this);
 }
 
-Span::~Span()
+size_t const &Span::operator[](size_t index) const 
 {
-    std::cout << "Span" << " default destructor called." << std::endl;
+    if (index >= size || index < 0)
+        throw (std::out_of_range("unable to access element!"));
+    return (numbers[index]);
 }
+
+void Span::addNumber(size_t num)
+{
+    if (numbers.size() >= size)
+        throw (std::out_of_range("unable to add more elements!"));
+    numbers.push_back(num);
+}
+
+void Span::addNumber(iterator begin, iterator end)
+{
+    for (iterator it = begin; it < end; it++)
+        addNumber(*it);
+}
+
+size_t Span::shortestSpan(void)
+{
+    if (numbers.size() < 2)
+        throw (noSpan());
+    iterator    min1 = std::min_element(numbers.begin(), numbers.end());
+    size_t      min2 = *std::max_element(numbers.begin(), numbers.end());
+    
+    for (iterator  it = numbers.begin() ; it != numbers.end() - 1 ; it++)
+        if (it != min1 && *it < min2)
+            min2 = *it;
+    return (min2 - *min1);
+}
+
+size_t Span::longestSpan(void)
+{
+    if (numbers.size() < 2)
+        throw (noSpan());
+    return (*std::max_element(numbers.begin(), numbers.end()) 
+            - *std::min_element(numbers.begin(), numbers.end()));
+}
+
+const char *Span::noSpan::what() const throw()
+{
+    return ("No span can be found!!");
+}
+
+Span::~Span(){}
--- cpp_module_08/ex01/Span.h
+++ cpp_module_08/ex01/Span.h
@@ -1,16 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eabdelha <eabdelha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 08:34:28 by eabdelha          #+#    #+#             */
+/*   Updated: 2022/08/11 10:27:43 by eabdelha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SPAN_H
 #define SPAN_H
 
 # include <iostream>
 # include <iomanip>
 # include <string>
+# include <vector>
+
+typedef std::vector<size_t>::iterator iterator;
 
 class Span
 {
+    std::vector<size_t>    numbers;
+    size_t              size;
     public:
-        Span();
+        Span(size_t n);
         Span(const Span &span);
         Span &operator = (const Span &span);
+        size_t const &operator[](size_t index) const ;
+        void addNumber(size_t num);
+        void addNumber(iterator begin, iterator end);
+        size_t shortestSpan(void);
+        size_t longestSpan(void);
+        class noSpan : public std::exception {
+          const char *what() const throw();  
+        };
         ~Span();
 };
 
